1edf0135a965292f0e6a541562b248d4e5f6b984
David835 <47478915+David835@users.noreply.github.com>
2021-06-25 17:07:32 -0500

    Add files via upload

--- pages.py
+++ pages.py
@@ -28,12 +28,12 @@ class Instructions1(Page):
 	form_fields = ["question_1", "question_2"]
 
 	def question_1_error_message(self, value):
-		if value == "Falsch":
-			return "Bitte lesen Sie die Instruktionen erneut genau durch und korrigieren Sie Ihre Antwort."
+		if value == "Incorrecto":
+			return "Lea las instrucciones nuevamente con atención y corrija su respuesta"
 
 	def question_2_error_message(self, value):
-		if value == "Richtig":
-			return "Bitte lesen Sie die Instruktionen erneut genau durch und korrigieren Sie Ihre Antwort."
+		if value == "Correcto":
+			return "Lea las instrucciones nuevamente con atención y corrija su respuesta."
 
 class Instructions2(Page):
 	form_model = "player"
@@ -41,11 +41,11 @@ class Instructions2(Page):
 
 	def question_3_error_message(self, value):
 		if value != 20:
-			return "Bitte lesen Sie die Instruktionen erneut genau durch und korrigieren Sie Ihre Antwort."
+			return "Lea las instrucciones nuevamente con atención y corrija su respuesta."
 
 	def question_4_error_message(self, value):
 		if value != 4:
-			return "Bitte lesen Sie die Instruktionen erneut genau durch und korrigieren Sie Ihre Antwort."
+			return "Lea las instrucciones nuevamente con atención y corrija su respuesta."
 
 
 class Instructions3(Page):
@@ -53,12 +53,12 @@ class Instructions3(Page):
 	form_fields = ["question_5", "question_6"]
 
 	def question_5_error_message(self, value):
-		if value == "Falsch":
-			return "Bitte lesen Sie die Instruktionen erneut genau durch und korrigieren Sie Ihre Antwort."
+		if value == "Incorrecto":
+			return "Lea las instrucciones nuevamente con atención y corrija su respuesta."
 
 	def question_6_error_message(self, value):
-		if value == "Richtig":
-			return "Bitte lesen Sie die Instruktionen erneut genau durch und korrigieren Sie Ihre Antwort."
+		if value == "Correcto":
+			return "Lea las instrucciones nuevamente con atención y corrija su respuesta."
 
 
 class CategoryPick(Page):
@@ -80,12 +80,16 @@ class CategoryWaitPage(WaitPage):
 
 	def after_all_players_arrive(self):
 		self.subsession.set_groups()
-		# weil wait_for_all_groups gesetzt ist, wird die Funktion nur einmal aufgerufen,
-		# folglich muss ich dann noch eine Ebene über die Gruppe hinaus auf die Subsession
-		# in der aufgerufenen Funktion auf der Subsession-Ebene wird dann durch alle Gruppen gegangen
-		# und da dann jeweils durch jeden Spieler um die Category zu kommunizieren.
+		# debido a que wait_for_all_groups está configurado, la función solo se llama una vez, 
+		# en consecuencia, tengo que ir un nivel más allá del grupo a la subsesión 
+		# en la función llamada en el nivel de subsesión pasa por todos los grupos 
+		# y luego a través de comunicarme con cada jugador sobre la categoría.
 		self.subsession.communicate_categories()
 
+
+class Agent_0(Page):
+	form_model = "player"
+	form_fields = ["investments"]
 
 class Agent(Page):
 	form_model = "player"
@@ -125,28 +129,6 @@ class Results_Agents(Page):
 		return self.player.role() == "Agent"
 
 
-class Questionnaire(Page):
-	form_model = "player"
-	form_fields = ["age", "gender", "studies", "nonstudent", "financial_advice", "income"]
-
-	# This works now, but is not in compliance with the oTree manual.. I guess we found a bug.
-	# returns an error message if a participant...
-	def error_message(self, values):
-		# ... indicates field of studies and ticks the box "non-student".
-		if values["studies"]:
-			if values["nonstudent"]:
-				return "Bitte geben Sie entweder ein Studienfach an oder wählen Sie \"Kein Student\""
-		# ... states no field of studies and and does not tick the box.
-		else:
-		#elif "studies" not in values:
-			if not values["nonstudent"]:
-				return "Sie haben kein Studienfach angegeben. Wenn Sie kein Student sind, treffen Sie bitte eine entsprechende Auswahl."
-
-	def before_next_page(self):
-		self.player.create_gender_dummies()
-		self.player.create_econ_dummy()
-		self.player.create_category_dummies()
-
 class Last_Page(Page):
 	pass
 
@@ -160,11 +142,11 @@ page_sequence = [
 	Instructions3,
 	CategoryPick,
 	CategoryWaitPage,
+	Agent_0,
 	Agent,
 	WaitForAgents,
 	Results_Principals,
 	WaitForPrincipals,
 	Results_Agents,
-	Questionnaire,
 	Last_Page
 ]
